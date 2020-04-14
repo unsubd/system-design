983cf645f44fb9d3019e4b234839d8f5e0e2b19f
Adit Pilai <aditapillai@gmail.com>
2020-04-14 07:04:09 +0530

    (refactor) : cleanup duplicate code

--- snakes-and-ladders/src/main/java/com/aditapillai/projects/snakesandladders/utils/GameUtils.java
+++ snakes-and-ladders/src/main/java/com/aditapillai/projects/snakesandladders/utils/GameUtils.java
@@ -27,31 +27,10 @@ public class GameUtils {
         snakes.add(new Snake(60, 9));
         snakes.add(new Snake(77, 25));
 
-        Square[] squares = IntStream.rangeClosed(0, 100)
-                                    .mapToObj(index -> new Square())
-                                    .toArray(Square[]::new);
-
-        snakes.forEach(snake -> squares[snake.getMouth()].setSnake(snake));
-        ladders.forEach(ladder -> squares[ladder.getStart()].setLadder(ladder));
-
-        Board board = new Board(squares);
-
-        Set<Player> players = new HashSet<>();
-        players.add(new Player("Player 1", Color.RED));
-        players.add(new Player("Player 2", Color.YELLOW));
-        players.add(new Player("Player 3", Color.GREEN));
-        players.add(new Player("Player 4", Color.BLUE));
-
-        Game game = Game.builder()
-                        .board(board)
-                        .players(players)
-                        .status(Status.CREATED)
-                        .build();
-
-        return game;
+        return GameUtils.createGame(snakes, ladders);
     }
 
-    public Game createGame(Set<Snake> snakes, Set<Ladder> ladders) {
+    public static Game createGame(Set<Snake> snakes, Set<Ladder> ladders) {
         Square[] squares = IntStream.rangeClosed(0, 100)
                                     .mapToObj(index -> new Square())
                                     .toArray(Square[]::new);
